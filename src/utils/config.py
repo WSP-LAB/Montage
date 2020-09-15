aecabfdfd1d2d2361a5bfbc8627d62e70f7526d7
leeswimming <sevendays37@gmail.com>
2020-09-16 07:58:59 +0900

    [Config] Update configuration

--- src/utils/config.py
+++ src/utils/config.py
@@ -6,13 +6,26 @@ from utils import read
 class Config:
   def __init__(self, conf_path):
     conf = self.load_conf(conf_path)
+    self.batch_size = conf['model']['batch_size']
+    self.bug_dir = conf['bug_dir']
     self.data_dir = conf['data_dir']
+    self.emb_size = conf['model']['emb_size']
     self.eng_name = conf['eng_name']
     self.eng_path = conf['eng_path']
+    self.epoch = conf['model']['epoch']
+    self.gamma = conf['model']['gamma']
+    self.lr = conf['model']['lr']
+    self.max_ins = conf['max_ins']
+    self.model_path = conf['model_path']
+    self.momentum = conf['model']['momentum']
+    self.split_size = conf['model']['split_size']
+    self.weight_decay = conf['model']['weight_decay']
+    self.num_gpu = conf['num_gpu']
     self.num_proc = conf['num_proc']
-    self.timeout = conf['timeout']
     self.opt = conf['opt']
     self.seed_dir = conf['seed_dir']
+    self.timeout = conf['timeout']
+    self.top_k = conf['top_k']
 
     self.ast_dir = os.path.join(self.data_dir, 'ast')
     self.log_dir = os.path.join(self.data_dir, 'log')
