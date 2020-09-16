59f979b1cf407228ad24a26b7e1fa456fccd3835
leeswimming <sevendays37@gmail.com>
2020-09-16 14:40:16 +0900

    [Main] Add options for Phase II & III

--- src/main.py
+++ src/main.py
@@ -2,9 +2,24 @@ import argparse
 import os
 import signal
 import sys
+import torch
 
+from fuzz.fuzz import fuzz
 from preprocess.preprocess import Preprocessor
+from train.train import ModelTrainer
 from utils.config import Config
+from utils.logger import print_msg
+
+def exec_fuzz(conf):
+  fuzz(conf)
+
+def exec_preprocess(conf):
+    preprocessor = Preprocessor(conf)
+    preprocessor.preprocess()
+
+def exec_train(conf):
+  trainer = ModelTrainer(conf)
+  trainer.train()
 
 def get_args():
   arg_parser = argparse.ArgumentParser()
@@ -16,7 +31,12 @@ def get_args():
 def handler(sigint, frame):
   os._exit(1)
 
-if __name__ == '__main__':
+def main():
+  if not torch.cuda.is_available():
+    print_msg('Montage only supports CUDA-enabled machines',
+              'ERROR')
+    sys.exit(1)
+
   # Add a SIGINT handler
   signal.signal(signal.SIGINT, handler)
 
@@ -27,5 +47,12 @@ if __name__ == '__main__':
   config_path = args.config
   conf = Config(config_path)
 
-  p = Preprocessor(conf)
-  p.preprocess()
+  if args.opt == 'preprocess':
+    exec_preprocess(conf)
+  elif args.opt == 'train':
+    exec_train(conf)
+  elif args.opt == 'fuzz':
+    exec_fuzz(conf)
+
+if __name__ == '__main__':
+  main()
